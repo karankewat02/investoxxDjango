e17b61dd85fd93f81ca89e42f1b3260ad58b7867
Karan Kewat <84654445+karankewat02@users.noreply.github.com>
2024-05-11 15:14:53 +0530

    dd

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python -m pip install -r requirements.txt
-python manage.py runserver --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py runserver --noinput --clear
 echo " BUILD END"
