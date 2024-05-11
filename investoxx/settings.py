105256416a3d8c1d918ff374461a6e4d2990c1a8
Karan Kewat <84654445+karankewat02@users.noreply.github.com>
2024-05-12 00:58:27 +0530

    fff

--- investoxx/settings.py
+++ investoxx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q*z)fdqj%%r^nup1#h99jf*9l)tx@w2(bk_w32_mh-z1qwnc@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,7 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
